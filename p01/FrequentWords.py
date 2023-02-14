8225bcb89a7d46a4944abd8c99136d2a9ae8a5b6
Israel Gonzalez <igonzals@gmail.com>
2023-02-14 14:48:29 -0700

    First commit

---
+++ p01/FrequentWords.py
@@ -0,0 +1,16 @@
+# FrequentWordsByCounting.py
+# FrequentWords() algorithm in class based on the PatternCount() method.
+
+class FrequentKmers:
+
+  def __init__(self, word=""):
+    self.word = word
+  
+  def count_by_counting(self):
+    return None
+  
+  def count_by_hashing(self):
+    return None
+
+  def generate_dna(self):
+    return None
