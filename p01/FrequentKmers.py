ea1bf8b54fe7fc460162c6482e367e7c5a0f1476
Israel Gonzalez <igonzals@gmail.com>
2023-02-20 15:27:41 -0700

    task 2 dome

--- p01/FrequentKmers.py
+++ p01/FrequentKmers.py
@@ -6,30 +6,31 @@ class FrequentKmers:
   def __init__(self, word=""):
     self.word = word
   
-  def count_by_counting(self):
+  def frequent_words(self, text, k):
     return None
   
   #see slide ch1 96
   def better_frequent_words(self, text, k):
     freq_map = self.frequent_map(text, k)
     max_count = max(freq_map.values())
-    for pattern in freq_map:
-      print(pattern)
-    return None
+    answer = []
+    for pattern in freq_map.items():
+      if pattern[1] == max_count:
+        answer.append(pattern[0])
+    return answer
   
   def frequent_map(self, text, k):
     freq_map = {}
     n = len(text)
     for i in range(n - k + 1):
       pattern = text[i:i+k]
-      if pattern in freq_map:
-        freq_map[pattern] += 1
-      else:
-        freq_map[pattern] = 1
+      if pattern is not None:
+        if pattern in freq_map:
+          freq_map[pattern] += 1
+        else:
+          freq_map[pattern] = 1
     return freq_map
 
   def generate_dna(self, L):
-    nucleotides = ['A', 'C', 'G', 'T']
-    probabilities = [0.25, 0.25, 0.25, 0.25]
-    sequence = np.random.choice(nucleotides, size=L, p=probabilities)
+    sequence = np.random.choice(['A', 'C', 'G', 'T'], size=L, p=[0.25, 0.25, 0.25, 0.25])
     return ''.join(sequence)
--- p01/main.py
+++ p01/main.py
@@ -2,11 +2,21 @@ from FrequentKmers import FrequentKmers
 
 if __name__ == '__main__':
   fk = fk = FrequentKmers()
+  print("===============================")
+  print("Part 1: Frequent Words counting")
+  print("===============================")
+  frequent_words = fk.frequent_words("ACGTTTCACGTTTTACGG", 3)
+  print(frequent_words)
+
   #see slide ch1 64
-  print("Part 2: Frequent Words")
+  print("===============================")
+  print("Part 2: Frequent Words hashing")
+  print("===============================")
   frequent_words = fk.better_frequent_words("ACGTTTCACGTTTTACGG", 3)
   print(frequent_words)
 
+  print("===============================")
   print("Part 3: Generating Sequence")
+  print("===============================")
   sequence = fk.generate_dna(10)
   print(sequence)
