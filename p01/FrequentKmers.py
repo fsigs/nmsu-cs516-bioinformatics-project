8570c0400210be0fc7e92f6d632aa7f601064f1b
Israel Gonzalez <igonzals@gmail.com>
2023-02-20 15:41:46 -0700

    last changes in class

--- p01/FrequentKmers.py
+++ p01/FrequentKmers.py
@@ -1,5 +1,6 @@
 # FrequentKmers.py
 import numpy as np
+from timeit import default_timer as timer
 
 class FrequentKmers:
 
@@ -7,17 +8,25 @@ class FrequentKmers:
     self.word = word
   
   def frequent_words(self, text, k):
-    return None
+    start = timer()
+    frequent_words = []
+    
+    #your code here
+    
+    end = timer()
+    return [frequent_words,  round((end - start) * 1000, 3)]
   
   #see slide ch1 96
   def better_frequent_words(self, text, k):
+    start = timer()
     freq_map = self.frequent_map(text, k)
     max_count = max(freq_map.values())
-    answer = []
+    frequent_words = []
     for pattern in freq_map.items():
       if pattern[1] == max_count:
-        answer.append(pattern[0])
-    return answer
+        frequent_words.append(pattern[0])
+    end = timer()
+    return [frequent_words,  round((end - start) * 1000, 3)]
   
   def frequent_map(self, text, k):
     freq_map = {}
@@ -34,3 +43,6 @@ class FrequentKmers:
   def generate_dna(self, L):
     sequence = np.random.choice(['A', 'C', 'G', 'T'], size=L, p=[0.25, 0.25, 0.25, 0.25])
     return ''.join(sequence)
+  
+  def read_covid_txt_file(self):
+    None
