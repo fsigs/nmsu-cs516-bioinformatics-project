00462d48953fc00da4987cb21249e8ebfbc125a4
Israel Gonzalez <igonzals@gmail.com>
2023-02-21 22:23:33 -0700

    tasks 1 to e2

--- p01/FrequentKmers.py
+++ p01/FrequentKmers.py
@@ -1,5 +1,7 @@
-# FrequentKmers.py
+# FrequentKmers.py Class
+import re
 import numpy as np
+import random
 from timeit import default_timer as timer
 
 class FrequentKmers:
@@ -7,44 +9,101 @@ class FrequentKmers:
   def __init__(self, word=""):
     self.word = word
   
+  # Frequent Words Algorithm methods:
   def frequent_words(self, text, k):
     start = timer()
     frequent_words = []
-    
-    #your code here
-
+    n = len(text)
+    count_array = [0] * (n - k + 1)
+    for i in range(n - k + 1):
+      pattern = text[i:i+k]
+      count_array[i] = self.pattern_count(text, pattern)
+    frequent_patterns = [text[i:i+k] for i in self.max_array(count_array)]
+    frequent_words = self.remove_duplicates(frequent_patterns)
     end = timer()
     return [frequent_words,  round((end - start) * 1000, 3)]
   
-  #see slide ch1 96
+  def pattern_count(self, text, pattern):
+    count = 0
+    for i in range(len(text) - len(pattern) + 1):
+      if text[i:i+len(pattern)] == pattern:
+        count += 1
+    return count
+  
+  def remove_duplicates(self, arr):
+    answer = []
+    for item in arr:
+      if item not in answer:
+        answer.append(item)
+    return answer
+
+  def max_array(self, arr):
+    max_val = max(arr)
+    max_indices = [i for i, val in enumerate(arr) if val == max_val]
+    return max_indices
+
+  # Better Frequent Words Algorithm methods:
   def better_frequent_words(self, text, k):
     start = timer()
     freq_map = self.frequent_map(text, k)
-    max_count = max(freq_map.values())
-    frequent_words = []
-    for pattern in freq_map.items():
-      if pattern[1] == max_count:
-        frequent_words.append(pattern[0])
-    end = timer()
-    return [frequent_words,  round((end - start) * 1000, 3)]
+    if freq_map is not None:
+      max_count = max(freq_map.values())
+      frequent_words = []
+      for pattern in freq_map.items():
+        if pattern[1] == max_count:
+          frequent_words.append(pattern[0])
+      end = timer()
+      return [frequent_words, round((end - start) * 1000, 3)]
+    else:
+      end = timer()
+      return[None, round((end - start) * 1000, 3)]
   
   def frequent_map(self, text, k):
     freq_map = {}
     n = len(text)
     for i in range(n - k + 1):
       pattern = text[i:i+k]
-      if pattern is not None:
-        if pattern in freq_map:
-          freq_map[pattern] += 1
-        else:
-          freq_map[pattern] = 1
+      if pattern in freq_map:
+        freq_map[pattern] += 1
+      else:
+        freq_map[pattern] = 1
     return freq_map
 
+  # Random DNA generator method:
   def generate_dna(self, L):
-    sequence = np.random.choice(['A', 'C', 'G', 'T'], size=L, p=[0.25, 0.25, 0.25, 0.25])
-    return ''.join(sequence)
+    return ''.join(np.random.choice(['A', 'C', 'G', 'T'], size=L, p=[0.25, 0.25, 0.25, 0.25]))
   
-  def get_covid_genome(self):
-    with open('SARS-CoV-2.txt', 'r') as file:
-      lines = file.read().replace('\n', '')
-    return lines
+  # Remover of polyA tail
+  def remove_polyA(self, sequence, tail_length):
+    pattern = "A{" + str(tail_length) + ",}$"
+    match = re.search(pattern, sequence)
+    if match:
+      sequence = sequence[:(match.start())]
+    return sequence
+  
+  # Reader of Covid genome files:
+  def get_covid_genome(self, filename):
+    with open(filename, 'r') as file:
+      sequence = file.read().replace('\n', '').strip()
+    tail_length = 10
+    if(filename == "SARS-CoV-2.txt"):
+      tail_length = 32
+    if(filename == "SARS-CoV.txt"):
+      tail_length = 23
+    return self.remove_polyA(sequence, tail_length)
+
+  # Permutation test (null distribution):
+  def permutation_test(self, text, k, num_perm):
+    p_value = None
+    fm = self.frequent_map(text, k)
+    max_count = max(fm.values())
+    null_population = list(text)
+    null_distribution = []
+    for _ in range(num_perm):
+      random.shuffle(null_population)
+      fm2 = self.frequent_map(''.join(null_population), k)
+      null_counts = max(fm2.values())
+      null_distribution.append(null_counts)
+    sum_ = sum(null_elem >= max_count for null_elem in null_distribution)
+    p_value = (sum_ + 1) / (num_perm + 1)
+    return p_value
--- p01/main.py
+++ p01/main.py
@@ -1,39 +1,139 @@
+import pprint
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 from FrequentKmers import FrequentKmers
 
 if __name__ == '__main__':
+  
+  pp = pprint.PrettyPrinter(indent=2)
   fk = fk = FrequentKmers()
-  print("===============================")
-  print("Part 1: Frequent Words counting")
-  print("===============================")
+
+  print()
+  print("============================================")
+  print("Task 1: Algorithm Frequent Words (counting)")
+  print("============================================")
+  print("Using test cases from class slides (Ch 1 slides 64 and 99), "
+        "\nwe show the list of frequent words and running time in ms for our implementation are correct:")
+  #test case: from slide 64 ch1 ppt
   frequent_words = fk.frequent_words("ACGTTTCACGTTTTACGG", 3)
-  print(frequent_words)
+  pp.pprint(frequent_words)
+  #test case: from slide 99 ch1 ppt
+  frequent_words = fk.frequent_words("atcaatgatcaacgtaagcttctaagcatgatcaaggtgctcacacagtttatccacaacctgagtggatgacatcaagataggtcgttgtatctccttcctctcgtactctcatgaccacggaaagatgatcaagagaggatgatttcttggccatatcgcaatgaatacttgtgacttgtgcttccaattgacatcttcagcgccatattgcgctggccaaggtgacggagcgggattacgaaagcatgatcatggctgttgttctgtttatcttgttttgactgagacttgttaggatagacggtttttcatcactgactagccaaagccttactctgcctgacatcgaccgtaaattgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaagatcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtttccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc", 9)
+  pp.pprint(frequent_words)
 
-  #see slide ch1 64
-  print("===============================")
-  print("Part 2: Frequent Words hashing")
-  print("===============================")
+  print()
+  print("=================================================")
+  print("Task 2: Algorithm Better Frequent Words (hashing)")
+  print("=================================================")
+  print("Using test cases from class slides (Ch 1 slides 64 and 99)," 
+        "\nwe show the list of frequent words and running time in ms for our implementation are correct:")
+  #test case: from slide 64 ch1 ppt
   frequent_words = fk.better_frequent_words("ACGTTTCACGTTTTACGG", 3)
-  print(frequent_words)
+  pp.pprint(frequent_words)
+  #test case: from slide 99 ch1 ppt
+  frequent_words = fk.better_frequent_words("atcaatgatcaacgtaagcttctaagcatgatcaaggtgctcacacagtttatccacaacctgagtggatgacatcaagataggtcgttgtatctccttcctctcgtactctcatgaccacggaaagatgatcaagagaggatgatttcttggccatatcgcaatgaatacttgtgacttgtgcttccaattgacatcttcagcgccatattgcgctggccaaggtgacggagcgggattacgaaagcatgatcatggctgttgttctgtttatcttgttttgactgagacttgttaggatagacggtttttcatcactgactagccaaagccttactctgcctgacatcgaccgtaaattgataatgaatttacatgcttccgcgacgatttacctcttgatcatcgatccgattgaagatcttcaattgttaattctcttgcctcgactcatagccatgatgagctcttgatcatgtttccttaaccctctattttttacggaagaatgatcaagctgctgctcttgatcatcgtttc", 9)
+  pp.pprint(frequent_words)
 
+  print()
+  print("========================================")
+  print("Task 3: Generating Random DNA Sequences")
+  print("========================================")
+  num_executions = 10
+  random_length = 30
+  print(f"Using Numpy random choice function of Python,"
+        "\nwe generate {num_executions} random DNA sequences of variable lenght: ")
+  for _ in range(num_executions):
+    random_sequence = fk.generate_dna(np.random.randint(random_length))
+    pp.pprint(random_sequence)  
+  '''
+  print()
   print("===============================")
-  print("Part 3: Generating Sequence")
+  print("Task 4: Ploting runtime for K & L")
   print("===============================")
-  sequence = fk.generate_dna(10)
-  print(sequence)
+  dna_length = [25, 50, 75, 100, 250, 500, 750, 1000]
+  dna_kmers = [3, 6, 9, 12, 15]
+  print("Using Pandas and Matplot libraries of Python, \nwe plot the running time of our two algorithms \nfor different sequence lengths and different k-mers:")
+  print("L = ", dna_length)
+  print("K-mers: ", dna_kmers)
+  
+  random_results = []
+  for l in dna_length:
+    s = fk.generate_dna(l)
+    pies_fwt = []
+    pies_bfwt = []
+    indexes = []
+    for k in dna_kmers:
+      result = {}
+      result["l"] = l
+      result["k"] = k
+      fwt = fk.frequent_words(s, k)[1]
+      result["fwt"] = fwt
+      bfwt = fk.better_frequent_words(s, k)[1]
+      result["bfwt"] = bfwt
+      random_results.append(result)
+      
+      indexes.append(k)
+      pies_fwt.append(fwt)
+      pies_bfwt.append(bfwt)
+    
+    plotdata = pd.DataFrame({
+      "runtime_fwt": pies_fwt,
+      "runtime_bfwt": pies_bfwt
+      }, index=indexes)
 
-  print("===============================")
-  print("Part 4: Ploting runtime for K & L")
-  print("===============================")
-  print("Pending....")
+    ax = plotdata.plot(kind = "bar")
+    for p in ax.patches:
+      ax.annotate(str(p.get_height()), (p.get_x() * 1.005, p.get_height() * 1.005))
+    plt.title("Frequent and Best Frequent word time with L = " + str(l))
+    plt.xlabel("K length")
+    plt.ylabel("Runtime (ms)")
+    plt.axhline(y=np.nanmean(fwt), color='blue', linestyle='--')
+    plt.axhline(y=np.nanmean(bfwt), color='orange', linestyle='--')
+    plt.savefig(f"./docs/images/q4_{l}.png", dpi=300)
+    
+  print("\nThe following is a table of the executions included in the charts:\n")
+  print("|Length|K-mer|FWT(ms)|BFWT(ms)|")
+  print("|:----:|:---:|:-----:|:------:|")
+  for r in random_results:
+    print("|", r["l"], "|", r["k"], "|", r["fwt"], "|", r["bfwt"], "|") 
+  ''' 
 
-  print("===============================")
-  print("Part 5: Reading Covid Genome")
-  print("===============================")
-  genome = fk.get_covid_genome()
-  k_values = [3, 4, 6, 9, 12, 15]
-  for k in k_values:
-    print("****************** ")
-    print(f"Frequent {k}-mers ")
-    print("****************** ")
-    print(fk.better_frequent_words(genome, k))
+  print()
+  print("============================================================")
+  print("Tasks 5 and E2: SARS-CoV-2 and SARS-CoV most frequent k-mers")
+  print("============================================================")
+  
+  print("\nThe following is a table of the most frequent k-mers present in the SARS-CoV-2 and SARS-CoV genomes:\n")
+  print("|Genome|K-mer|String|")
+  print("|:----:|:---:|:-----:|")
+
+  file_names = ['SARS-CoV-2', 'SARS-CoV']
+  covid_k = [3, 6, 9, 12, 15]
+  for file in file_names: 
+    covid2_genome = fk.get_covid_genome(file + '.txt')
+    for k in covid_k:
+      result = fk.better_frequent_words(covid2_genome, k)
+      rs = result[0]
+      for r in rs:
+        print("|", file, "|", k, "|", r, "|")
+
+  # Task 6
+  # Use Blast: https://blast.ncbi.nlm.nih.gov/Blast.cgi
+  # https://www.youtube.com/watch?v=WRKQGwh_Mw0
+  # https://www.news-medical.net/health/How-Does-the-SARS-Virus-Genome-Compare-to-Other-Viruses.aspx#:~:text=All%20bind%20to%20the%20ACE2,in%20SARS%2DCoV%2D2.
+
+  print()
+  print("================================================================================")
+  print("E1: Null Distribution of the most frequent k-mers in SARS-CoV-2 genome sequence")
+  print("================================================================================")
+  covid2_genome = fk.get_covid_genome('SARS-CoV-2.txt')
+  k_mer = 3
+  num_per = 1000
+  print(f"Using the SARS-CoV-2 genome sequence, our permutation test \nwith {num_per} permutations gives:")
+  for k_mer in covid_k:
+    p_value = fk.permutation_test(covid2_genome, k_mer, num_per)
+    print(f"{k_mer}-mer gives a p-value of {p_value}")
+  exit()
+
   
