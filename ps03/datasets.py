01d298aa9e5bc443ae6dc688b150b39b4348f48b
Israel Gonzalez <igonzals@gmail.com>
2023-04-18 08:36:49 -0600

    first steps

---
+++ ps03/datasets.py
@@ -0,0 +1,21 @@
+datasets = {
+  "Coronavirus": {
+    "NC_045512.2": {"title":"Coronavirus 2 isolate Wuhan-Hu-1", "poly": "A"},
+    "KT225476.2": {"title":"Middle East respiratory syndrome coronavirus isolate MERS-CoV/THA/CU/17_06_2015", "poly": None},
+    "MZ937003.2": {"title":"Bat coronavirus isolate BANAL-20-236/Laos/2020", "poly": "A"},
+    "AY545919.1": {"title":"SARS coronavirus isolate CFB/SZ/94/03", "poly": "A"},
+    "OM009282.1": {"title":"Pangolin coronavirus HKU4/P251T/pangolin/2018", "poly": "A"}
+  },
+  "HIV-1": {
+
+  },
+  "Adenovirus-2": {
+
+  },
+  "Ebolavirus": {
+
+  },
+  "Hepatitis-B": {
+
+  }
+}
